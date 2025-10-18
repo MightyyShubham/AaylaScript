3f314f7e20ccb2be2e1c0c4384ff2b37e260625e
MightyyShubham <94773217+MightyyShubham@users.noreply.github.com>
2025-10-18 20:15:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.post("/submitContact", async (req, res) => {
   try {
     const googleUrl = "https://script.google.com/macros/s/AKfycbwGOowVWQWXloWQi6tWIPfgEL4PuzqlB_4olpyzd-GqKVd1FE-phF5cqXMQ0y9gpbBR/exec"; // Replace with your Apps Script Web App URL
+    console.log(req.body);
     const response = await fetch(googleUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -25,12 +26,12 @@ app.post("/submitContact", async (req, res) => {
 app.post("/submitCustom", async (req, res) => {
   try {
     const googleUrl = "https://script.google.com/macros/s/AKfycbzC1QvuX5GE6j09v8tgj0HYHHOaEkqFACaPSWpqRAKAEr9gYBK2Xibslg8Fx0sEx6M9/exec"; // Replace with your Apps Script Web App URL
+    console.log(req.body);
     const response = await fetch(googleUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
     });
-
     const text = await response.text();
     res.status(200).send(text);
   } catch (err) {
@@ -41,12 +42,13 @@ app.post("/submitCustom", async (req, res) => {
 app.post("/submitFeedback", async (req, res) => {
   try {
     const googleUrl = "https://script.google.com/macros/s/AKfycbwd-Z_FpNaz9Y-NK5Jzh8RlfTmCb5gyz_z_JwQ1wwMNIQN6k4CCDfyEiVJp8kaxYfm_/exec"; // Replace with your Apps Script Web App URL
+    
+    console.log(req.body);
     const response = await fetch(googleUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
     });
-
     const text = await response.text();
     res.status(200).send(text);
   } catch (err) {
