fab47a3a311fb6cbd106f2b5464745a392d85021
MightyyShubham <94773217+MightyyShubham@users.noreply.github.com>
2025-10-18 09:05:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.post("/submit", async (req, res) => {
   try {
-    const googleUrl = "https://script.google.com/macros/s/AKfycbxxxxx/exec"; // Replace with your Apps Script Web App URL
+    const googleUrl = "https://script.google.com/macros/s/AKfycbxZHu8u7CViBQSvwNizVGYpEFGv8e1mDw31atiVytrRGO9uPKXB4t-4NZAnPm5PKrqi/exec"; // Replace with your Apps Script Web App URL
 
     const response = await fetch(googleUrl, {
       method: "POST",
