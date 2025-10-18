a51aa3021de621852b7ec0a528374f5ce983fe87
MightyyShubham <94773217+MightyyShubham@users.noreply.github.com>
2025-10-18 19:58:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/submit", async (req, res) => {
+app.post("/submitContact", async (req, res) => {
   try {
-    const googleUrl = "https://script.google.com/macros/s/AKfycbyWY-Hd1_DhK-855X-PGZB7lL_jTTwEhRxIlH06acCyXcSrnXVI4VgwpxU-NUEsI4JW/exec"; // Replace with your Apps Script Web App URL
-    console.log("req",req);
+    const googleUrl = "https://script.google.com/macros/s/AKfycbwGOowVWQWXloWQi6tWIPfgEL4PuzqlB_4olpyzd-GqKVd1FE-phF5cqXMQ0y9gpbBR/exec"; // Replace with your Apps Script Web App URL
+    const response = await fetch(googleUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+    });
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/submitCustom", async (req, res) => {
+  try {
+    const googleUrl = "https://script.google.com/macros/s/AKfycbzC1QvuX5GE6j09v8tgj0HYHHOaEkqFACaPSWpqRAKAEr9gYBK2Xibslg8Fx0sEx6M9/exec"; // Replace with your Apps Script Web App URL
+    const response = await fetch(googleUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+    });
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/submitFeedback", async (req, res) => {
+  try {
+    const googleUrl = "https://script.google.com/macros/s/AKfycbwd-Z_FpNaz9Y-NK5Jzh8RlfTmCb5gyz_z_JwQ1wwMNIQN6k4CCDfyEiVJp8kaxYfm_/exec"; // Replace with your Apps Script Web App URL
     const response = await fetch(googleUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
