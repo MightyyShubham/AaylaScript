110dec5a4370f053c636f3eaf52e2b91a67f5f81
MightyyShubham <94773217+MightyyShubham@users.noreply.github.com>
2026-01-17 23:06:47 +0530

    Add /submitExportDetails endpoint
    Added a new endpoint to submit export details to a Google Apps Script.

--- server.js
+++ server.js
@@ -57,5 +57,23 @@ app.post("/submitFeedback", async (req, res) => {
   }
 });
 
+//this is for Export PAge
+app.post("/submitExportDetails", async (req, res) => {
+  try {
+    const googleUrl = "https://script.google.com/macros/s/AKfycbwFY0zW_eSwix9a0b1FJK_TQeDQHmj3IJszoV8ozcASq0RgxYIuLi4-9FDGpBDuPT9cTQ/exec"; // Replace with your Apps Script Web App URL
+    
+    console.log(req.body);
+    const response = await fetch(googleUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+    });
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
